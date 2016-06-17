690c7ade7e55960486dc6e7f2164f4c6921af144
yaelbrumer <yael.brumer@gmail.com>
2016-06-17 09:45:24 +0300

    Add notification messages

--- RSfinalProject/RecSys.cs
+++ RSfinalProject/RecSys.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,17 +33,35 @@ namespace RSfinalProject
 
         public void TrainCpModel()
         {
+            Stopwatch timer = Stopwatch.StartNew();
+
             predictionEngine.TrainCpModel();
+
+            timer.Stop();
+            TimeSpan elapsed = timer.Elapsed;
+            Console.WriteLine("Train Cp Model was completed successfully\nExection Time: " + elapsed.ToString("mm':'ss':'fff"));
         }
 
         public void TrainSeqModel()
         {
+            Stopwatch timer = Stopwatch.StartNew();
+
             predictionEngine.TrainSeqModel();
+
+            timer.Stop();
+            TimeSpan elapsed = timer.Elapsed;
+            Console.WriteLine("Train Seq Model was completed successfully\nExection Time: " + elapsed.ToString("mm':'ss':'fff"));
         }
 
         public void ComputeHitRatio()
         {
+            Stopwatch timer = Stopwatch.StartNew();
+
             evaluationEngine.ComputeHitRatio();
+
+            timer.Stop();
+            TimeSpan elapsed = timer.Elapsed;
+            Console.WriteLine("Compute Hit Ratio was completed successfully\nExection Time: " + elapsed.ToString("mm':'ss':'fff"));
         }
     }
 }
